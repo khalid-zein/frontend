f92208f393470cdcdbc162ea6c07c5a3e699a980
khalid-zein <zeinkhalid004@gmail.com>
2023-04-01 16:48:48 +0300

    almost done

---
+++ src/pages/Login.jsx
@@ -0,0 +1,78 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Container, Row, Col } from "reactstrap";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+
+const Login = ({ setUser }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("https://food-api-ivzo.onrender.com/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((user) => {
+          console.log(user);
+          setUser(user);
+          navigate("/foods");
+        });
+      }
+    });
+  }
+
+  return (
+    <>
+      <Helmet title="Login">
+        <div className="md:flex justify-center items-center md:mt-16 mt-12 mx-8">
+          <CommonSection title="Login" />
+
+          <section>
+            <Container>
+              <Row>
+                <Col lg="6" md="6" sm="12" className="m-auto text-center">
+                  <form className="form mb-5" onSubmit={handleSubmit}>
+                    <h1 className="md:text-2xl text-xl my-4 font-semibold text-gray-800">
+                      Login
+                    </h1>
+                    <div className="form__group">
+                      <label htmlFor="email">Email</label>
+                      <br />
+                      <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                    <div className="form__group">
+                      <label htmlFor="password">Password</label>
+                      <br />
+                      <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                    <button className="addTOCart__btn">Login</button>
+                  </form>
+                  <Link to="/register">
+                    Don't have an account? Create an account
+                  </Link>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </div>
+      </Helmet>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/routes/Routers.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import AllFoods from "../pages/AllFoods";
+import FoodDetails from "../pages/FoodDetails";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import { useState } from "react";
+import Reviews from "../components/UI/reviews/reviews";
+
+const Routers = () => {
+  const [user, setUser] = useState(null);
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route
+        path="/foods"
+        element={<AllFoods user={user} setUser={setUser} />}
+      />
+      <Route path="/foods/:id" element={<FoodDetails />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/login" element={<Login setUser={setUser} />} />
+      <Route path="/register" element={<Register setUser={setUser} />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/reviews" element={<Reviews />} />
+    </Routes>
+  );
+};
+
+export default Routers;
